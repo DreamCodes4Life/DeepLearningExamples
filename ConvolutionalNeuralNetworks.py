aec40d12474e44641cbf65aa2a8ccbc716c768ce
Borja <105612077+DreamCodes4Life@users.noreply.github.com>
2025-11-26 00:32:39 +0000

    Implement CNN for image classification with PyTorch
    This script implements a Convolutional Neural Network (CNN) for image classification using PyTorch. It includes data preparation, model definition, training, and validation processes.

---
+++ ConvolutionalNeuralNetworks.py
@@ -0,0 +1,152 @@
+# Prep data specifically for a CNN
+# Create a more sophisticated CNN model, understanding a greater variety of model layers
+# Train a CNN model and observe its performance
+
+import torch.nn as nn
+import pandas as pd
+import torch
+from torch.optim import Adam
+from torch.utils.data import Dataset, DataLoader
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+torch.cuda.is_available()
+
+# Preparing Images
+train_df = pd.read_csv("data/asl_data/sign_mnist_train.csv")
+valid_df = pd.read_csv("data/asl_data/sign_mnist_valid.csv")
+
+#
+sample_df = train_df.head().copy()  # Grab the top 5 rows: https://www.photoshopessentials.com/essentials/rgb/
+sample_df.pop('label')
+sample_x = sample_df.values
+sample_x
+
+sample_x.shape
+
+IMG_HEIGHT = 28
+IMG_WIDTH = 28
+IMG_CHS = 1 # grey scale, only one color scale
+
+sample_x = sample_x.reshape(-1, IMG_CHS, IMG_HEIGHT, IMG_WIDTH)
+sample_x.shape
+
+# Create a Dataset
+class MyDataset(Dataset):
+    def __init__(self, base_df):
+        x_df = base_df.copy()  # Some operations below are in-place
+        y_df = x_df.pop('label')
+        x_df = x_df.values / 255  # Normalize values from 0 to 1
+        x_df = x_df.reshape(-1, IMG_CHS, IMG_WIDTH, IMG_HEIGHT)
+        self.xs = torch.tensor(x_df).float().to(device)
+        self.ys = torch.tensor(y_df).to(device)
+
+    def __getitem__(self, idx):
+        x = self.xs[idx]
+        y = self.ys[idx]
+        return x, y
+
+    def __len__(self):
+        return len(self.xs)
+
+
+# Create a DataLoader
+BATCH_SIZE = 32
+
+train_data = MyDataset(train_df)
+train_loader = DataLoader(train_data, batch_size=BATCH_SIZE)
+train_N = len(train_loader.dataset)
+
+valid_data = MyDataset(valid_df)
+valid_loader = DataLoader(valid_data, batch_size=BATCH_SIZE)
+valid_N = len(valid_loader.dataset)
+
+train_loader = DataLoader(train_data, batch_size=BATCH_SIZE, shuffle=True)
+
+# Let's grab a batch from the DataLoader to make sure it works.
+batch = next(iter(train_loader))
+batch
+
+#  let's check the shapes to be sure.
+batch[0].shape
+batch[1].shape
+
+# Creating a Convolutional Model
+n_classes = 24
+kernel_size = 3
+flattened_img_size = 75 * 3 * 3
+
+model = nn.Sequential(
+    # First convolution
+    nn.Conv2d(IMG_CHS, 25, kernel_size, stride=1, padding=1),  # 25 x 28 x 28
+    nn.BatchNorm2d(25),
+    nn.ReLU(),
+    nn.MaxPool2d(2, stride=2),  # 25 x 14 x 14
+    # Second convolution
+    nn.Conv2d(25, 50, kernel_size, stride=1, padding=1),  # 50 x 14 x 14
+    nn.BatchNorm2d(50),
+    nn.ReLU(),
+    nn.Dropout(.2),
+    nn.MaxPool2d(2, stride=2),  # 50 x 7 x 7
+    # Third convolution
+    nn.Conv2d(50, 75, kernel_size, stride=1, padding=1),  # 75 x 7 x 7
+    nn.BatchNorm2d(75),
+    nn.ReLU(),
+    nn.MaxPool2d(2, stride=2),  # 75 x 3 x 3
+    # Flatten to Dense
+    nn.Flatten(),
+    nn.Linear(flattened_img_size, 512),
+    nn.Dropout(.3),
+    nn.ReLU(),
+    nn.Linear(512, n_classes)
+)
+
+# Summarizing the Model
+model = torch.compile(model.to(device))
+model
+
+loss_function = nn.CrossEntropyLoss()
+optimizer = Adam(model.parameters())
+
+def get_batch_accuracy(output, y, N):
+    pred = output.argmax(dim=1, keepdim=True)
+    correct = pred.eq(y.view_as(pred)).sum().item()
+    return correct / N
+
+# Training the Model
+def train():
+    loss = 0
+    accuracy = 0
+
+    model.train()
+    for x, y in train_loader:
+        output = model(x)
+        optimizer.zero_grad()
+        batch_loss = loss_function(output, y)
+        batch_loss.backward()
+        optimizer.step()
+
+        loss += batch_loss.item()
+        accuracy += get_batch_accuracy(output, y, train_N)
+    print('Train - Loss: {:.4f} Accuracy: {:.4f}'.format(loss, accuracy))
+
+
+def validate():
+    loss = 0
+    accuracy = 0
+
+    model.eval()
+    with torch.no_grad():
+        for x, y in valid_loader:
+            output = model(x)
+
+            loss += loss_function(output, y).item()
+            accuracy += get_batch_accuracy(output, y, valid_N)
+    print('Valid - Loss: {:.4f} Accuracy: {:.4f}'.format(loss, accuracy))
+
+epochs = 20
+
+for epoch in range(epochs):
+    print('Epoch: {}'.format(epoch))
+    train()
+    validate()
+
